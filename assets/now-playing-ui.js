7f105836e30a1954855f8e9b12dace629f6d9886
Nathaniel Albrecht <nathanielalbrecht@gmail.com>
2022-08-23 03:08:08 -0400

    fixes

--- assets/now-playing-ui.js
+++ assets/now-playing-ui.js
@@ -43,7 +43,7 @@
     function startProgressClock(progress, duration)
     {
         const controller = new AbortController();
-        animationInterval(1000, controller, function(){
+        animationInterval(1000, controller.signal, () => {
             const progressString = formatProgress(progress++, duration);
             console.log(progressString);
             progressElement.textContent = ` (${progressString})`;
@@ -66,23 +66,23 @@
         // Prefer currentTime, as it'll better sync animtions queued in the
         // same frame, but if it isn't supported, performance.now() is fine.
         const start = document.timeline ? document.timeline.currentTime : performance.now();
-      
+        
         function frame(time) {
-          if (signal.aborted) return;
-          callback(time);
-          scheduleFrame(time);
+            if (signal.aborted) return;
+            callback(time);
+            scheduleFrame(time);
         }
-      
+        
         function scheduleFrame(time) {
-          const elapsed = time - start;
-          const roundedElapsed = Math.round(elapsed / ms) * ms;
-          const targetNext = start + roundedElapsed + ms;
-          const delay = targetNext - performance.now();
-          setTimeout(() => requestAnimationFrame(frame), delay);
+            const elapsed = time - start;
+            const roundedElapsed = Math.round(elapsed / ms) * ms;
+            const targetNext = start + roundedElapsed + ms;
+            const delay = targetNext - performance.now();
+            setTimeout(() => requestAnimationFrame(frame), delay);
         }
-      
+        
         scheduleFrame(start);
-      }
+    }
 
 })();
 
